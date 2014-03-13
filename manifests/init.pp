ab61cfbf2c186d8e9a456092b37c5d7a91de1153
root <root@d1stkfactory>
2014-03-13 08:53:51 +0000

    Fix other errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,16 +2,36 @@
 #
 #INSTALL SKYPE ON DESKTOP
 #
-class bumblebee ($driver_name = "nvidia-current", $package_name = "bumblee-nvidia"){
+class bumblebee ($driver_name = "nvidia-current", $package_name = "bumblee-nvidia", $user = "root"){
     #Install package
     package { $package_name: ensure   => installed,}
-
+    
+    #Fix for usergroup
+    exec {"usermod -a -G bumblebee ${user}": path => ["/usr/bin", "/usr/sbin"],}
+    
+    #bbswitch
+    #package {"": ensure => installed,}
+    
     #Change xorg.conf (acer v3571g)
     file { "/etc/bumblebee/bumblebee.conf":
         replace => "yes",
         owner   => "root",
         mode    => 644,
-        require => Package[$package_name],
         content => template('bumblebee/bumblebee.conf'),
+    }
+
+    #Nvdia xorg conf
+    file { "/etc/bumblebee/xorg.conf.nvidia":
+        replace => "yes",
+        owner   => "root",
+        mode    => 644,
+        content => template('bumblebee/xorg.conf.nvidia'),
+    }
+    #Xorg.conf
+    file { "/etc/X11/xorg.conf":
+        replace => "yes",
+        owner   => "root",
+        mode    => 644,
+        content => template('bumblebee/xorg.conf'),
     }
 }
